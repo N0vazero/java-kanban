ae91a516dbf1b888f82d48552c3b1c2b8907b196
Zero <n0vazero@yandex.ru>
2025-07-26 15:47:09 +0300

    Test for HistoryList Created

--- test/util/HistoryListTest.java
+++ test/util/HistoryListTest.java
@@ -1,4 +1,83 @@
+package util;
+
+import manager.Managers;
+import manager.TaskManager;
+import org.junit.jupiter.api.BeforeAll;
+import task.Status;
+import task.Task;
+
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 class HistoryListTest {
-  
+    static TaskManager manager = Managers.getDefalut();
+    static Task task1 = new Task("Отработать смену на заводе", "8:00 - 17:00", Status.DONE);
+    static Task task2 = new Task("Отправиться в бар", null, Status.IN_PROGRESS);
+    static Task task3 = new Task("Лечь спать");
+
+    @BeforeAll
+    public static void preparation() {
+        manager.addTask(task1);
+        manager.addTask(task2);
+        manager.addTask(task3);
+    }
+    @Test
+    public void listRemoveElements() {
+        HistoryList taskList = new HistoryList();
+
+        taskList.addLast(task1);
+        taskList.addLast(task2);
+        taskList.addLast(task3);
+
+        Task deletedTask = task2;
+        taskList.remove(deletedTask);
+
+        for (Task task : taskList) {
+            assertNotEquals(task, deletedTask);
+        }
+    }
+
+    @Test
+    public void listAddElements() {
+        HistoryList taskList = new HistoryList();
+
+        taskList.addLast(task1);
+        taskList.addLast(task2);
+        taskList.addLast(task3);
+
+        Task addedTask = new Task("New");
+        manager.addTask(addedTask);
+        taskList.addLast(addedTask);
+
+        assertEquals(taskList.getLast(), addedTask);
+    }
+
+    @Test
+    public void firstAddedAreFirst() {
+        HistoryList taskList = new HistoryList();
+
+        taskList.addLast(task1);
+        Task firstAdded = task1;
+        taskList.addLast(task2);
+        taskList.addLast(task3);
+
+        assertEquals(taskList.getFirst(), firstAdded);
+    }
+
+    @Test
+    public void secondAfterFirstRemovedBecomesFirst() {
+        HistoryList taskList = new HistoryList();
+
+        taskList.addLast(task1);
+        Task firstAdded = task1;
+
+        taskList.addLast(task2);
+        Task secondAdded = task2;
+
+        taskList.addLast(task3);
+
+        taskList.remove(firstAdded);
+
+        assertEquals(taskList.getFirst(), secondAdded);
+    }
 }
